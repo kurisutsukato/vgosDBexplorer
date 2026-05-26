504671ee5722ff0288cd84d8b77cda3d166be9a5
Christian K. <ckkart@gmail.com>
2026-05-26 16:28:55 -0300

    added hostname

--- visor.py
+++ visor.py
@@ -785,4 +785,4 @@ def update_plotpol(st1, st2, source, parameter, relayout, parameter2, session_id
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9003)
+    app.run(debug=True, host='0.0.0.0', port=9003)
